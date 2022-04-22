7927dae004d8bd480adb4423bc54818f588009a0
shubham <shubhamaggarwal3964@gmail.com>
2022-04-23 03:32:34 +0530

    Updates

---
+++ precache-manifest.a72c3cba5128a3189afbe61da82a59d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253f6eb36d860845263df806171e91d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "085cc4ec3b2acdf04454",
+    "url": "/static/css/main.e141296e.chunk.css"
+  },
+  {
+    "revision": "a8fa0fc6419624aee851",
+    "url": "/static/js/2.6cdbf0dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6cdbf0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "085cc4ec3b2acdf04454",
+    "url": "/static/js/main.6a91b995.chunk.js"
+  },
+  {
+    "revision": "5268b1c7be79030bb5b5",
+    "url": "/static/js/runtime-main.142b1330.js"
+  },
+  {
+    "revision": "c537d7d180515a2d14e723821dcf5097",
+    "url": "/static/media/loadRocket.c537d7d1.gif"
+  }
+]);
